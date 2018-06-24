cd716a4dcb22e8207e65a0e12108d8f9a6b3ec6f
Amir <amir.gevari@gmail.com>
2018-06-24 17:17:31 +0100

    added a new path

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get('/' , (req , res) =>{
     res.send('Hello World from amir macbook')
 })
 
+app.get('/:name' , (req , res) =>{
+    var name = req.params.name
+    res.send(`Hello World from ${name}  macbook`)
+})
 app.listen(2525 , ()=>console.log('Example app listening on port 2525!') )
