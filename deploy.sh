5959e9fd7b4125ba3292c9848533d2edd760351b
Amir <amir.gevari@gmail.com>
2018-06-24 17:38:02 +0100

    try to deploy

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@ EC2_USER="ec2-user"
 KEY_FILE="~/.ssh/devopstest.pem"
 
 mkdir ~/.ssh
+chmod 700 ~/.ssh
 echo "$PKEY" >> $KEY_FILE
+chmod 600 $KEY_FILE
 ssh $HOST -l $EC2_USER -i "$KEY_FILE" "sudo docker pull $IMAGE:$VERSION && sudo docker stop $CONTAINER && sudo docker rm $CONTAINER && sudo docker run --name $CONTAINER $IMAGE:$VERSION"
