20562486a0aba1ec1c17e82870a47e98bdc8d39c
Amir <amir.gevari@gmail.com>
2018-06-25 12:58:07 +0100

    added ssh key

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,5 @@ chmod 700 ~/.ssh
 echo "$PKEY" >> $KEY_FILE
 chmod 600 $KEY_FILE
 
+
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $HOST -l $EC2_USER -i "$KEY_FILE" "sudo docker pull $DOCKER_USER/deploymentamir:$VERSION && sudo docker stop $CONTAINER && sudo docker rm $CONTAINER && sudo docker run --name $CONTAINER $DOCKER_USER/deploymentamir:$VERSION"
