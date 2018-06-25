ae14d935c5e2126c17320a9052d6484967c5c75a
Amir <amir.gevari@gmail.com>
2018-06-25 15:38:10 +0100

    change the deploy .sh file

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,6 @@
 
 HOST="34.243.242.180"
 EC2_USER="ec2-user"
-# # KEY_FILE="~/.ssh/"
-# CONTAINER="amircontainer"
-# KEY_FILE="/home/ubuntu/.ssh/id_34.243.242.180"
-
-# chmod 700 ~/.ssh
-# # echo "$PKEY" >> $KEY_FILE
-# chmod 600 $KEY_FILE
-
-
-# ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null 
-ssh -o StrictHostKeyChecking=no $HOST -l $EC2_USER -i  "sudo docker pull $DOCKER_USER/deploymentamir:$VERSION && sudo docker stop $CONTAINER && sudo docker rm $CONTAINER && sudo docker run --name $CONTAINER $DOCKER_USER/deploymentamir:$VERSION"
+PORT="2525"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $HOST -l $EC2_USER "sudo docker stop $CONTAINER; sudo docker rm $CONTAINER; sudo docker create -p $PORT:$PORT --name $CONTAINER $DOCKER_USER/$CONTAINER:$VERSION && sudo docker start $CONTAINER"
+# ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $HOST -l $EC2_USER -i  "sudo docker pull $DOCKER_USER/deploymentamir:$VERSION && sudo docker stop $CONTAINER && sudo docker rm $CONTAINER && sudo docker run --name $CONTAINER $DOCKER_USER/deploymentamir:$VERSION"
