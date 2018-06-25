09d19ac3a31f8cf39299a355218900d9b3f907fc
Amir <amir.gevari@gmail.com>
2018-06-25 13:51:57 +0100

    coment ssh key lines

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 HOST="34.243.242.180"
 EC2_USER="ec2-user"
-KEY_FILE="~/.ssh/"
+# KEY_FILE="~/.ssh/"
 CONTAINER="amircontainer"
 
 
 chmod 700 ~/.ssh
-echo "$PKEY" >> $KEY_FILE
-chmod 600 $KEY_FILE
+# echo "$PKEY" >> $KEY_FILE
+# chmod 600 $KEY_FILE
 
 
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $HOST -l $EC2_USER -i "$KEY_FILE" "sudo docker pull $DOCKER_USER/deploymentamir:$VERSION && sudo docker stop $CONTAINER && sudo docker rm $CONTAINER && sudo docker run --name $CONTAINER $DOCKER_USER/deploymentamir:$VERSION"
